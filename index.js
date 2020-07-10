2835503ff2b16dd003db542113ef3168dd5a21b2
saurabhrane1199 <36326161+saurabhrane1199@users.noreply.github.com>
2020-07-11 03:51:48 +0530

    PORT Error Fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const PORT = 4000;
+const PORT = process.env.PORT || 4000
 const morgan = require('morgan')
 const conn = require('./mysqlconn');
 
